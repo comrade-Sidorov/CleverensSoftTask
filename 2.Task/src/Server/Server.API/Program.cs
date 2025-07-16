2145d874e248b58f01d190d221f053998c53339a
psidorov.work <psidorov.work@gmail.com>
2025-07-16 16:06:23 +0300

    try ddd

---
+++ 2.Task/src/Server/Server.API/Program.cs
@@ -0,0 +1,8 @@
+using Server.Domain;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
---
+++ 2.Task/src/Server/Server.Domain/Count.cs
@@ -0,0 +1,16 @@
+﻿namespace Server.Domain;
+
+public class Count
+{
+    private int _count = 0;
+
+    public int GetCount()
+    {
+        return _count;
+    }
+
+    public void AddToCount()
+    {
+        _count++;   
+    }
+}
